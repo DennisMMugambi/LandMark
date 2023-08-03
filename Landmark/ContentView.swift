cef9c5cce4a355cf6375e1efb37ea0bbce1db273
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2023-08-03 15:35:41 +0300

    Combine all of the views into ContentView.swift

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -5,17 +5,35 @@ struct ContentView: View {
     var body: some View {
 
         VStack {
-            Text("Turtle rock")
-                .font(.title)
-            HStack {
-                Text("Joshua tree national park")
-                    .font(.subheadline)
-                Spacer()
-                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                    .font(.subheadline)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua tree national park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About turtle rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 
 }
